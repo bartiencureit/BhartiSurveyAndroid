ec63a3a8d278710f9521649e465e8dcb0558f31e
Admin <sappi.cd@gmail.com>
2022-03-01 11:11:25 +0530

    Added Some Helper files

---
+++ app/src/main/java/com/encureit/bhartisurveyandroid/Helpers/GlobalHelper.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) - 2020 & Created By AbhishekR - EncureIT :)
+ */
+
+package com.encureit.bhartisurveyandroid.Helpers;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.Settings;
+
+import com.encureit.bhartisurveyandroid.network.Contants;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class GlobalHelper {
+    public Context context;
+    private SharedPreferencesHelper sharedPreferencesHelper;
+    private UIHelper uiHelper;
+    private UserHelper userHelper;
+    //private ServerDataAccess serverDataAccess;
+    private ImageHelper imageHelper;
+//    private DataFields dataFields;
+//    private LocalDataAccess localDataAccess;
+    private GPSTracker gpsTracker;
+
+    public GlobalHelper(Context context) {
+        this.context = context;
+    }
+
+    public SharedPreferencesHelper getSharedPreferencesHelper() {
+        if (this.sharedPreferencesHelper == null) {
+            this.sharedPreferencesHelper = new SharedPreferencesHelper(this.context);
+        }
+        return this.sharedPreferencesHelper;
+    }
+
+//    public LocalDataAccess getLocalDataAccess() {
+//        if (localDataAccess == null) {
+//            localDataAccess = new LocalDataAccess(context);
+//        }
+//        return localDataAccess;
+//    }
+
+    public GPSTracker getGpsTracker() {
+        if (gpsTracker == null) {
+            gpsTracker = new GPSTracker(context);
+        }
+        return gpsTracker;
+    }
+
+    public String getDevicesID() {
+        return Settings.Secure.getString(this.context.getContentResolver(),Settings.Secure.ANDROID_ID);
+    }
+
+    public String getCurrentDateTime() {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return df.format(c.getTime());
+    }
+
+    public UIHelper getUIHelper() {
+        if (this.uiHelper == null) {
+            this.uiHelper = new UIHelper(this.context);
+        }
+        return uiHelper;
+    }
+
+    public UserHelper getUserHelper() {
+        if (this.userHelper == null) {
+            this.userHelper = new UserHelper(this.context);
+        }
+        return this.userHelper;
+    }
+
+    public ImageHelper getImageHelper() {
+        if (this.imageHelper == null) {
+            this.imageHelper = new ImageHelper(this.context);
+        }
+        return this.imageHelper;
+    }
+
+//    public ServerDataAccess getServerDataAccess() {
+//        if (this.serverDataAccess == null) {
+//            this.serverDataAccess = new ServerDataAccess(this.context);
+//        }
+//        return this.serverDataAccess;
+//    }
+//
+//    public DataFields getDataFields() {
+//        if (dataFields == null) {
+//            dataFields = new DataFields();
+//        }
+//        return dataFields;
+//    }
+
+    public static String md5(final String s) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++) {
+                String h = Integer.toHexString(0xFF & messageDigest[i]);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getVersionNumber() {
+        String version = "0.0";
+        PackageInfo pInfo = null;
+        try {
+            pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            version = pInfo.versionName;
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return version;
+    }
+
+    public String getRandomNumber() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+
+    public void writeTextFile(String text) {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("ddMMyyyy");
+            SimpleDateFormat timeformat = new SimpleDateFormat("hh:mm:ss");
+            Date date = new Date();
+            File root = new File(Environment.getExternalStorageDirectory(), Contants.FOLDER_SAVE);
+            if (!root.exists()) {
+                android.util.Log.e("WriteText", "Root Not Exists");
+                if (!root.mkdir()) {
+                    android.util.Log.e("WriteText", "Failed to create direcktory");
+                    return;
+                }
+            }
+
+            File fileLog = new File(root, "log-" + format.format(date) + ".txt");
+            if (!fileLog.exists()) {
+                android.util.Log.e("WriteText", "Can't found file Log");
+                if (!fileLog.createNewFile()) {
+                    android.util.Log.e("WriteText", "Can't create file Log");
+                    return;
+                }
+            }
+
+            BufferedWriter buf = new BufferedWriter(new FileWriter(fileLog, true));
+            final String currentDateTimeString = timeformat.format(new Date());
+            buf.append(currentDateTimeString + " : " + text);
+            buf.newLine();
+            buf.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        android.util.Log.i("WriteText", "write : " + text);
+    }
+
+    public boolean checkFileDirExternal() {
+        File folder = new File(Environment.getExternalStorageDirectory(), Contants.FOLDER_SAVE);
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdir();
+            return success;
+        } else {
+            return true;
+        }
+    }
+
+    public File createFileFromBitmap(Bitmap bitmap, String file_name) throws FileNotFoundException, IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+        File destination = null;
+        if (checkFileDirExternal()) {
+            destination = new File(Environment.getExternalStorageDirectory(), Contants.FOLDER_SAVE +
+                    Contants.DEFAULT_FILENAME + file_name + ".png");
+            FileOutputStream fo;
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        }
+        return destination;
+    }
+}
--- app/src/main/java/com/encureit/bhartisurveyandroid/network/Contants.java
+++ app/src/main/java/com/encureit/bhartisurveyandroid/network/Contants.java
@@ -1,6 +1,31 @@
 package com.encureit.bhartisurveyandroid.network;
 
+import android.os.Environment;
+
 public class Contants {
     public static final String BASE_URL = "https://machinetest.encureit.com";
     public static final String GET_LIST = "country.php";
+
+    public static String PREF_IS_LOGIN = "PREF_IS_LOGIN";
+    public static String PREF_AUTH_TOKEN = "PREF_AUTH_TOKEN";
+    public static String FOLDER_SAVE = "android/data/data/com.encureit.samtadoot/";
+    public static String DEFAULT_FILENAME = "encureit_aas_";
+    public static String TABLE_FORM = "dynamicfield";
+    public static String FOLDER_PATH = Environment.getExternalStorageDirectory().getPath() + "/" + FOLDER_SAVE;
+
+    public static String APP_LOGIN_USER_ID = "com.encureit.samtadoot.SYNC_LOGIN_USER_ID";
+    public static String APP_USER_ID = "com.encureit.samtadoot.SYNC_USER_ID";
+    public static String APP_LOGIN_USER_ROLE = "com.encureit.samtadoot.SYNC_LOGIN_USER_ROLE";
+    public static String SYNC_LOGIN_DATE_TIME_DATA = "com.encureit.samtadoot.SYNC_LOGIN_DATE_TIME_DATA";
+    public static String SYNC_DATE_TIME_ALL_DATA = "com.encureit.samtadoot.all_data_sync_date_time";
+    public static String SYNC_DATE_TIME_CANDIDATE_DATA = "com.encureit.samtadoot.candidate_data_sync_date_time";
+
+    public static String SURVEY_MASTER_ID = "com.encureit.samtadoot.surveyMasterId";
+    public static String SURVEY_SECTION_ID = "com.encureit.samtadoot.surveySection_ID";
+    public static String SECTION_NO_NAME = "com.encureit.samtadoot.sectionNoName";
+
+    public static String FORM_TYPE = "com.encureit.samtadoot.formType";
+    public static String UNIQUE_FORM_ID = "com.encureit.samtadoot.uniqueFormID";
+    public static String IS_FORM_NEW = "com.encureit.samtadoot.isFormNew";
+    public static String IS_FORM_COMPLETED = "com.encureit.samtadoot.isFormCompleted";
 }
